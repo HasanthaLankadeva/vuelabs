4b303e6a09447f1fb645225419a8a199f606dfde
HasanthaLankadeva <hasantha88@gmail.com>
2024-03-18 01:49:45 +0530

    lab 2 updates

---
+++ src/pages/LabTwo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  beforeCreate() {
+    console.log("the component is creating...");
+  },
+  created() {
+    console.log("the component is created...");
+  },
+  beforeMount() {
+    console.log("the component is loding...");
+  },
+  mounted() {
+    console.log("the component is loded...");
+  },
+  beforeUpdate() {
+    console.log("the component is updating...");
+  },
+  updated() {
+    console.log("the component is updated...");
+  },
+};
+</script>
